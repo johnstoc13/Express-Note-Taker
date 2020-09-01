e40142bb2193dccf3a8644debd45157ba57ce1e2
johnstoc13 <66090689+johnstoc13@users.noreply.github.com>
2020-09-01 10:48:54 -0400

    styling changes

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -116,7 +116,7 @@ const renderNoteList = (notes) => {
 
     if (withDeleteButton) {
       const $delBtn = $(
-        "<i class='fas fa-trash-alt float-right text-danger delete-note'>"
+        "<i class='fas fa-trash-alt float-right orange delete-note'>"
       );
       $li.append($delBtn);
     }
